2ffcfa3d3c56ff7d048e80c0fd170f07a0356e9c
iamaro80 <iamaro80@users.noreply.github.com>
2026-09-22 20:08:33 +0300

    Remove USE_EXACT_ALARM permission and bump versionCode to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "jo.aliftaa.prayertimes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

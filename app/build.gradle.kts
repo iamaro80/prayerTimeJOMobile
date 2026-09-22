1c52abe9c63e93ed539ed6a42ca728ad40e36757
iamaro80 <iamaro80@users.noreply.github.com>
2026-09-22 10:12:44 +0300

    Bump versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "jo.aliftaa.prayertimes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
